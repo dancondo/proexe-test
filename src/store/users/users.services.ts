ddfaf785cf2a459a025afb19d994b567b5f24fda
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2022-02-10 18:56:29 -0300

    create user services

---
+++ src/store/users/users.services.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+export const USERS_API_BASE_URL = process.env.REACT_APP_USERS_API_BASE_URL || '';
+
+export const getUsers = async (): Promise<UserDto[]> => {
+  const { data } = await axios.get(USERS_API_BASE_URL);
+  return data
+}
+
+export const getUser = async (userId: number): Promise<UserDto> => {
+  const { data } = await axios.get(`${USERS_API_BASE_URL}/${userId}`)
+  return data
+}
+
+export const createUser = async (userDto: UpsertUserDto): Promise<UserDto> => {
+  const { data } = await axios.post(USERS_API_BASE_URL, { userDto })
+
+  return {
+    id: data.id,
+    ...data.userDto
+  }
+}
+
+export const updateUser = async(userId: number, user: Partial<UserDto>): Promise<UserDto> => {
+  const { data } = await axios.put(`${USERS_API_BASE_URL}/${userId}`, { user })
+  return {
+    id: data.id,
+    ...data.user
+  }
+}
+
+export const deleteUser = async(userId: number): Promise<void> => {
+  return await axios.delete(`${USERS_API_BASE_URL}/${userId}`)
+}
