95727a3e5c30e1d42352b748dbe60aadad8697a3
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2022-02-10 18:58:25 -0300

    creates edit user page

---
+++ src/pages/EditUser/index.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+
+import UserForm from 'components/Users/Form';
+import { useAppDispatch, useAppSelector } from 'hooks/state.hooks';
+import { UserCreators } from 'store/users/users.actions';
+import { useNavigate, useParams } from 'react-router-dom';
+import UsersLayout from 'layouts/UsersLayout';
+import { updateUser } from 'store/users/users.services';
+import { NotificationCreators } from 'store/notifications/notifications.actions';
+import Panel from 'components/Base/Panel';
+
+const EditUserPage: React.FC = () => {
+  const navigate = useNavigate()
+  const { userId } = useParams() 
+  const dispatch = useAppDispatch()
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    if (userId) {
+      dispatch(UserCreators.getUserDetails(userId))
+    }
+  }, [dispatch, userId])
+
+  const user = useAppSelector(state => state.users.userDetails)
+
+  const onSubmit = async (userDto: UpsertUserDto) => {
+    if (!userId) {
+      return
+    }
+    try {
+      setLoading(true)
+      const data = await updateUser(+userId, userDto)
+      dispatch(UserCreators.updateUser(data))
+      dispatch(NotificationCreators.showNotification({
+        timeout: 5000,
+        type: 'success',
+        title: 'User Updated',
+        message: `User #${userId} was updated successfully!`,
+      }))
+      navigate('/users')
+    } catch (err) {
+      dispatch(NotificationCreators.showNotification({
+        message: err,
+        timeout: 5000,
+        type: 'danger',
+        title: 'Edit user error!',
+      }))
+    } finally {
+      setLoading(false)
+    }
+
+  }
+
+  const onCancel = () => {
+    navigate('/users')
+  }
+
+  return (
+    <UsersLayout
+      title={`Edit User #${userId}`}
+    >
+      {
+        !user && (
+          <Panel>
+            <span
+              className="display-6 text-center"
+            >
+              Loading...
+            </span>
+          </Panel>
+        )
+      }
+      {
+        user && (
+          <UserForm
+            user={user}
+            loading={loading}
+            onSubmit={onSubmit}
+            onCancel={onCancel}
+          />
+        )
+      }
+    </UsersLayout>
+  )
+}
+
+export default EditUserPage;
