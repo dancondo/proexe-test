e30f6ba379e53dc70559d64ab5da8fe63361e7d3
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2022-02-10 18:59:09 -0300

    register app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
+import UsersPage from 'pages/Users';
+import EditUserPage from 'pages/EditUser';
+import CreateUserPage from 'pages/CreateUser';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+      <Routes>
+        <Route
+          path="users"
+          element={<UsersPage />}
+        />
+        <Route
+          path="users/create"
+          element={<CreateUserPage />}
+        />
+        <Route
+          path="users/:userId/edit"
+          element={<EditUserPage />}
+        />
+      </Routes>
     </div>
   );
 }
